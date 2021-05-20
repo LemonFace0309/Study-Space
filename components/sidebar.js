b978d42459fbf6adb7d126fa602b3f7ac23e31bd
Mabel <mabelkwok322@gmail.com>
2021-05-19 21:49:04 -0400

    ugly purple, will edit later

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,88 +1,54 @@
-import React from 'react'
-import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import ClearAllIcon from '@material-ui/icons/ClearAll';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import SyncAltIcon from '@material-ui/icons/SyncAlt';
-import LayersIcon from '@material-ui/icons/Layers';
-import LockIcon from '@material-ui/icons/Lock';
-import EcoIcon from '@material-ui/icons/Eco';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import LayersIcon from "@material-ui/icons/Layers";
 
 const Sidebar = () => {
-    return (
-        <div className="md:w-3/12 w-6/12 h-screen shadow-2xl">
-            <div className=" border-b py-3 mt-1 flex justify-around ">
-                <p className="text-xl  font-semibold">elrond</p>
-                <p>|</p>
-                <p className="text-gray-400 text-lg">wallet</p>
-
+  return (
+    //<div class="grid grid-flow-col grid-cols-100 grid-rows-1 gap-4">
+    <div className="md: w-4/12 h-screen shadow-2xl bg-gradient-to-br from-purple-900 to-purple-800">
+      <div className="border-b py-3 mt-1 flex justify-around">
+        <p className="text-xl  font-semibold">picture</p>
+        <p>|</p>
+        <p className="text-gray-400 text-lg">wallet</p>
+      </div>
+      <div className="p-4 space-y-14">
+        <div className="space-y-4">
+          <h1 className="text-gray-400">Menu</h1>
+          <div className="">
+            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-white-600  cursor-pointer  ">
+              <HomeIcon className=" text-gray-300" />
+              <p className="text-gray-300">Home</p>
             </div>
-            <div className="p-4 space-y-14">
-                <div className="space-y-4" >
-                    <h1 className="text-gray-400">Menu</h1>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <DonutLargeIcon className=" text-gray-300" />
-                            <p className=" "  >Dashbord</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <ClearAllIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Transaction</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <ArrowUpwardIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Send</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <ArrowDownwardIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Receive</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <SyncAltIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Buy</p>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="space-y-6" >
-                    <h1 className="text-gray-400">Delegation</h1>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <LockIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Delegate</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <EcoIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Claim Reward</p>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="space-y-6" >
-                    <h1 className="text-gray-400">taking    </h1>
-                    <div className="">
-                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-                            <LayersIcon className="text-gray-300" />
-                            <p className="text-gray-600  " >Validate</p>
-                        </div>
-                    </div>
-
-
-                </div>
+          </div>
+          <div className="">
+            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-white-600  cursor-pointer  ">
+              <SearchIcon className="text-gray-300" />
+              <p className="text-gray-300">Browse</p>
             </div>
-
+          </div>
+          <div className="">
+            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-white-600  cursor-pointer  ">
+              <EqualizerIcon className="text-gray-300" />
+              <p className="text-gray-300">Statistics</p>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="">
+          <div className="space-y-6 align-baseline">
+            <h1 className="text-gray-300">FRIEND ACTIVITY</h1>
+            <div className=""></div>
+            <div
+              className="transform cursor-pointer transition delay-100 w-3/12  p-12 py-20 shadow-xl  
+                    border rounded-xl bg-gradient-to-r from-purple-400 to-purple-400"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    gradientColorStops: (theme) => ({
+      ...theme("colors"),
+      primary: "#4e3276",
+      secondary: "#bdacd4",
+    }),
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
