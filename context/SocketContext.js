2dcf848cb160e69de50aafec5ccfa735955e7487
Charles Liu <charles.liu0309@gmail.com>
2021-05-22 17:36:09 -0400

    added call answer and end logic in socket context

--- context/SocketContext.js
+++ context/SocketContext.js
@@ -1,16 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useContext } from 'react'
 import { io } from 'socket.io-client'
+import Peer from 'simple-peer'
 
 const SocketContext = React.createContext()
 
 const socket = io(process.env.NODE_SERVER || 'http://localhost:8080')
 
-const ContextProvider = ({ children }) => {
+export const useSocket = () => useContext(SocketContext)
+
+export const SocketProvider = ({ children }) => {
   const [stream, setStream] = useState(null)
   const [me, setMe] = useState('')
   const [call, setCall] = useState({})
+  const [callAccepted, setCallAccepted] = useState(false)
+  const [callEnded, setCallEnded] = useState(false)
+  const [name, setName] = useState('')
 
   const myVideo = useRef()
+  const userVideo = useRef()
+  const connectionRef = useRef()
 
   useEffect(async () => {
     const currentStream = await navigator.mediaDevices.getUserMedia({
@@ -29,9 +37,71 @@ const ContextProvider = ({ children }) => {
     })
   }, [])
 
-  const answerCall = () => {}
-  const callUser = () => {}
-  const leaveCall = () => {}
+  const answerCall = () => {
+    setCallAccepted(true)
 
-  return <SocketContext.Provider>{children}</SocketContext.Provider>
+    const peer = new Peer({ initiator: false, trickle: false, stream })
+    peer.on('signal', (data) => {
+      socket.emit('answercall', {
+        signal: data,
+        to: call.from,
+      })
+    })
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream
+    })
+
+    peer.signal(call.signal)
+
+    connectionRef.current = peer
+  }
+  const callUser = (id) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream })
+    peer.on('signal', (data) => {
+      socket.emit('calluser', {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name,
+      })
+    })
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream
+    })
+
+    socket.on('callaccepted', (signal) => {
+      setCallAccepted(true)
+      peer.signal(signal)
+    })
+
+    connectionRef.current = peer
+  }
+  const leaveCall = () => {
+    setCallEnded(true)
+
+    connectionRef.current.destroy()
+
+    window.location.reload()
+  }
+
+  return (
+    <SocketContext.Provider
+      value={{
+        call,
+        callAccepted,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        setName,
+        callEnded,
+        me,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import { Provider } from 'next-auth/client'
 
 import Layout from '../components/Layout/Layout'
+import { SocketProvider } from '../context/SocketContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SocketProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SocketProvider>
     </Provider>
   )
 }
