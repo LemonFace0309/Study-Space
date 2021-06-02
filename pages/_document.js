0170d571d322e1ac5b04e5441054e898f01fe01f
Charles Liu <charles.liu0309@gmail.com>
2021-06-02 04:06:40 -0400

    finished 80% of login boilerplate with no logic yet

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheets } from '@material-ui/core/styles'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -17,22 +22,25 @@ class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  });
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
 
-  const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
 
 export default MyDocument
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,26 +1,87 @@
-import { useState, useEffect } from 'react'
-import { Button } from '@material-ui/core'
-import { signIn, signOut, getSession } from 'next-auth/client'
+import { useState } from 'react'
+import Image from 'next/image'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Dialog from '@material-ui/core/Dialog'
+import { Box, Divider, Grid, Hidden, Typography } from '@material-ui/core'
 
-function Auth() {
-  const [loading, setLoading] = useState(true)
-  const [session, setSession] = useState()
+import styles from '../../styles/Auth/Auth.module.css'
 
-  useEffect(async () => {
-    const userSession = await getSession()
-    setSession(userSession)
-    setLoading(false)
-    console.log(userSession)
-  }, [])
+const Auth = () => {
+  const [modalOpen, setModalOpen] = useState(false)
 
-  const AuthButton = () => {
-    if (session) {
-      return <Button onClick={() => signOut()}>Signout</Button>
-    }
-    return <Button onClick={() => signIn()}>Login</Button>
+  const handleOpenModal = () => {
+    setModalOpen(true)
   }
 
-  return <>{!loading && <div><AuthButton /></div>}</>
+  const handleCloseModal = () => {
+    setModalOpen(false)
+  }
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleOpenModal}>
+        Login
+      </Button>
+      <Dialog
+        fullWidth={true}
+        maxWidth="lg"
+        open={modalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="Login/Signup Modal"
+        className={styles.Dialogue}
+      >
+        <Grid container className="my-8">
+          <Grid item xs={12} md={6}>
+            <div className="flex flex-col items-center p-8">
+              <Typography
+                variant="h4"
+                component="h1"
+                style={{ color: '#4E3276' }}
+                className="font-bold"
+              >
+                Log in
+              </Typography>
+              <div className="w-8/12 mt-8">
+                <TextField label="Email Address" fullWidth className="mb-1" />
+                <TextField label="Password" fullWidth />
+              </div>
+              <Button className="my-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-8/12">
+                LOG IN
+              </Button>
+              <div className="flex w-full justify-center items-center">
+                <Divider className="w-4/12" />
+                <Typography variant="caption" component="p" className="mx-3">OR</Typography>
+                <Divider className="w-4/12" />
+              </div>
+              <Typography variant="caption" component="p" className="my-2">Log in with</Typography>
+            </div>
+          </Grid>
+          <Hidden smDown>
+            <Grid
+              item
+              md={6}
+              className="overflow-hidden flex flex-col content-center items-start"
+            >
+              <Typography variant="h6" component="h4">
+                All your study needs in one space.
+              </Typography>
+              <div className="w-11/12 max-w-lg relative">
+                <div style={{ paddingTop: '100%' }}>
+                  <Image
+                    src="/images/placeholder.jpg"
+                    alt="login screen picture"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              </div>
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Dialog>
+    </div>
+  )
 }
 
 export default Auth
