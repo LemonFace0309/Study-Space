9c5441b484dc971d89c0ec587a4a1f2b51bdb1d1
Eden <edenchan717@gmail.com>
2021-06-07 14:05:11 -0400

    Add spacing

--- components/SpacesCard.js
+++ components/SpacesCard.js
@@ -1,40 +1,51 @@
 import React from "react";
-import {Paper} from "@material-ui/core";
-
+import { Paper, Container, Box, Typography } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 import MicIcon from "@material-ui/icons/Mic";
 import ChatIcon from "@material-ui/icons/Chat";
 import PeopleIcon from "@material-ui/icons/People";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { AddBoxOutlined, AddBoxRounded } from "@material-ui/icons";
 
 export default function SpacesCard(props) {
-  return (
-    <Paper className={`flex flex-col cursor-pointer w-4/12 transform hover:scale-110 hover:shadow transition ease-out duration-200`}
-    elevation={5}>
-      <div className="flex flex-grow justify-between ">
-        <div className="text-left p-3">
-          <h2 className="text-xl text-purple-900">{props.spaceName}</h2>
-          <p className="text-sm text-gray-600">{props.description}</p>
-        </div>
+  const theme = useTheme();
 
-        <div className=" text-purple-300 flex items-center justify-center flex-col m-1 bg-opacity-30">
+  return (
+    <Paper
+      className={`flex flex-col cursor-pointer w-4/12 transform hover:scale-110 hover:shadow transition ease-out duration-200`}
+      elevation={5}
+    >
+      <Box className="flex flex-grow justify-between ">
+        <Box color={theme.palette.primary.dark} className="text-left p-3">
+          <h2 className="text-xl">{props.spaceName}</h2>
+          <p className="text-sm text-gray-500">{props.description}</p>
+        </Box>
+
+        <Box
+          color={theme.palette.primary.main}
+          className=" flex items-center justify-center flex-col m-1 bg-opacity-30"
+        >
           <VideocamOffIcon />
           <MicIcon />
           <ChatIcon />
-        </div>
-      </div>
+        </Box>
+      </Box>
 
-      <div className="flex flex-row text-gray-900 text-sm text-purple-400 bg-purple-50 rounded-xl p-2 space-x-2">
-        <div>
+      <Box
+        bgcolor={theme.palette.primary.light}
+        color={theme.palette.primary.main}
+        className="flex flex-row text-sm rounded-xl p-2 space-x-2"
+      >
+        <Box>
           <PeopleIcon />
           {props.headCount}
-        </div>
-
-        <div>
+        </Box>
+        <Box>
           <LibraryMusicIcon />
           {props.music}
-        </div>
-      </div>
+        </Box>
+      </Box>
     </Paper>
   );
 }
