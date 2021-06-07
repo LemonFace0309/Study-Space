62e9dc2b7c257251586fa080c59889b3c8390327
edenchan@wawanesa.com <edenchan@wawanesa.com>
2021-06-07 19:15:54 -0400

    Add Timer to studySpaceDemo

--- pages/spaces/studySpaceDemo.js
+++ pages/spaces/studySpaceDemo.js
@@ -1,25 +1,28 @@
 import React from "react";
 import LinearProgress from "@material-ui/core/LinearProgress";
 
-import { Paper, Container, Box, Typography } from "@material-ui/core";
-import { useTheme, createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Paper, Container, Box, Grid, Typography } from "@material-ui/core";
+import {
+  useTheme,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 import MicIcon from "@material-ui/icons/Mic";
 import ChatIcon from "@material-ui/icons/Chat";
 import PeopleIcon from "@material-ui/icons/People";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
-
-
 const theme = createMuiTheme({
   palette: {
     primary: {
       dark: "#614885",
       main: "#977BBF",
+      medium: "#E5DDED",
       light: "#F5F2F9",
     },
     secondary: {
-      main: "#f44336",
+      main: "#8698A7",
     },
   },
 });
@@ -28,71 +31,95 @@ function TimeCard(props) {
   const theme = useTheme();
 
   return (
-    <Paper className={`flex flex-col`}>
-      <Box className="flex flex-grow justify-between ">
-        <Box color={theme.palette.primary.dark} className="text-left p-3">
-          <h2 className="text-xl">{props.spaceName}</h2>
-          <p className="text-sm text-gray-500">{props.description}</p>
-        </Box>
-
-        <Box
-          color={theme.palette.primary.main}
-          className=" flex items-center justify-center flex-col m-1 bg-opacity-30"
-        >
-          <VideocamOffIcon />
-          <MicIcon />
-          <ChatIcon />
-        </Box>
-      </Box>
-
+    <Paper elevate={10}>
       <Box
         bgcolor={theme.palette.primary.light}
-        color={theme.palette.primary.main}
-        className="flex flex-row text-sm rounded-xl p-2 space-x-2"
+        className="flex flex-col justify-center p-5 rounded-lg"
       >
-        <Box>
-          <PeopleIcon />
-          {props.headCount}
-        </Box>
-        <Box>
-          <LibraryMusicIcon />
-          {props.music}
-        </Box>
+        <Grid className="grid grid-cols-1">
+          <Box
+            color={theme.palette.secondary.main}
+            bgcolor={theme.palette.primary.medium}
+            className="text-center text-md uppercase tracking-tight font-bold rounded-lg w-11/12 p-3 m-2 "
+          >
+            Almost there! Take a break in
+          </Box>
+
+          <Container className="flex flex-col text-center justify-between ">
+            <Box
+              color={theme.palette.primary.dark}
+              className="text-6xl uppercase tracking-widest font-semibold"
+            >
+              01:15
+            </Box>
+            <Box
+              color={theme.palette.secondary.main}
+              className="flex flex-row justify-between m-auto space-x-10 text-sm uppercase tracking-tight font-semibold"
+            >
+              <h3>Hour</h3>
+              <h3>Minutes</h3>
+            </Box>
+          </Container>
+
+          <Container className="flex flex-col text-center justify-between ">
+            <Box
+              color={theme.palette.primary.dark}
+            
+            >
+              <LinearDeterminate />
+            </Box>
+            <Box
+              color={theme.palette.secondary.main}
+              className="flex flex-row justify-between text-sm tracking-tight font-semibold"
+            >
+              <h3>12:10 pm</h3>
+              <h3>2:30 pm</h3>
+            </Box>
+          </Container>
+        </Grid>
       </Box>
     </Paper>
   );
 }
 
 function LinearDeterminate() {
- 
   const [progress, setProgress] = React.useState(70);
 
-//   React.useEffect(() => {
-//     const timer = setInterval(() => {
-//       setProgress((oldProgress) => {
-//         if (oldProgress === 100) {
-//           return 0;
-//         }
-//         const diff = Math.random() * 10;
-//         return Math.min(oldProgress + diff, 100);
-//       });
-//     }, 500);
+  //   React.useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       setProgress((oldProgress) => {
+  //         if (oldProgress === 100) {
+  //           return 0;
+  //         }
+  //         const diff = Math.random() * 10;
+  //         return Math.min(oldProgress + diff, 100);
+  //       });
+  //     }, 500);
 
-//     return () => {
-//       clearInterval(timer);
-//     };
-//   }, []);
+  //     return () => {
+  //       clearInterval(timer);
+  //     };
+  //   }, []);
 
   return (
-    <div >
-      <LinearProgress variant="determinate" value={progress} />
+    <div>
+      <LinearProgress
+        color='primary'
+        variant="determinate"
+        value={progress}
+      />
     </div>
   );
 }
 
 export default function StudySpaceDemo() {
-    return (
-        <LinearDeterminate/>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Container className="w-5/12">
+          <TimeCard />
+        </Container>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
