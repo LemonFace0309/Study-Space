ba7221f2d547abf4bfd51af4e0cb6d32922ecef9
edenchan@wawanesa.com <edenchan@wawanesa.com>
2021-06-08 11:49:16 -0400

    Add Study Space Layout

--- pages/spaces/studySpaceDemo.js
+++ pages/spaces/studySpaceDemo.js
@@ -2,13 +2,12 @@ import React from "react";
 import Timer from "../../components/Spaces/StudySpace/Timer";
 import User from "../../components/Spaces/StudySpace/User";
 
-import { Paper, Container, Box, Grid, Typography, LinearProgress, Avatar } from "@material-ui/core";
+import { Container, Box, Grid, Button, Typography } from "@material-ui/core";
 import {
   useTheme,
   createMuiTheme,
   ThemeProvider,
 } from "@material-ui/core/styles";
-
 
 import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 import MicIcon from "@material-ui/icons/Mic";
@@ -17,7 +16,6 @@ import PeopleIcon from "@material-ui/icons/People";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import CreateIcon from "@material-ui/icons/Create";
 import SettingsIcon from "@material-ui/icons/Settings";
-
 
 const theme = createMuiTheme({
   palette: {
@@ -34,59 +32,90 @@ const theme = createMuiTheme({
   },
 });
 
-
-
-
 export default function StudySpaceDemo() {
   // For the sake of the demo, we are using locally defined theme.
   // When using ThemeProvider, use the useTheme() hook.
   // const theme = useTheme();
-  
+
   return (
     <ThemeProvider theme={theme}>
-      <Grid className="grid grid-cols-12 space-y-24">
-        <Box
-          color={theme.palette.primary.dark}
-          className="flex flex-row  rounded-full py-3 px-8 space-x-5 col-span-4"
-        >
-          <Box className="flex flex-row justify-start rounded-full border-2">
-            <CreateIcon />
-            <p className="font-bold uppercase tracking-tight">Session</p>
-          </Box>
-          <SettingsIcon />
-        </Box>
-        <Box className="col-span-4">
-          <Timer />
-        </Box>
-
-        <Box
-          color={theme.palette.secondary.quit}
-          className="flex flex-row justify-start rounded-full border-2 col-span-4"
-        >
-          <p className="font-bold">Quit</p>
-        </Box>
-        <Box
-          color={theme.palette.primary.dark}
-          className="flex flex-row space-x-5  justify-start items-end col-span-2 px-8"
-        >
-          <VideocamOffIcon />
-          <MicIcon />
-        </Box>
-        <Box className="col-span-8">
-          <Grid className="grid grid-cols-6 ">
-            <User /> <User /> <User /> <User /> <User /> <User />
-            <User /> <User /> <User /> <User /> <User /> <User />
+      <Container className="mt-10">
+        <Grid container className="grid grid-cols-12">
+          <Grid
+            item
+            color={theme.palette.primary.dark}
+            className="flex flex-row  rounded-full space-x-5 col-span-4"
+          >
+            <Box >
+              <Button
+                variant="outlined"
+                className="flex flex-row justify-start rounded-full  outline-none"
+              >
+                <CreateIcon />
+                <Typography variant="body1" className="uppercase">
+                  Session
+                </Typography>
+              </Button>
+            </Box>
+            <SettingsIcon />
           </Grid>
-        </Box>
-        <Box
-          color={theme.palette.primary.dark}
-          className="flex flex-row space-x-5 justify-end items-end col-span-2 px-8"
-        >
-          <LibraryMusicIcon />
-          <PeopleIcon />
-          <ChatIcon />
-        </Box>
-      </Grid>
+
+          <Grid item className="col-span-4">
+            <Container className="mb-24">
+              <Timer />
+            </Container>
+          </Grid>
+
+          <Grid
+            item
+            className="flex flex-row justify-end items-start col-span-4"
+          >
+            <Button
+              color="secondary"
+              variant="outlined"
+              className="rounded-full  outline-none"
+            >
+              <Typography variant="body1" className="uppercase">
+                Quit
+              </Typography>
+            </Button>
+          </Grid>
+
+   
+          <Grid item className="flex justify-start items-end col-span-2 px-8 ">
+            <Box
+              color={theme.palette.primary.dark}
+              className="flex flex-row space-x-5 "
+            >
+              <VideocamOffIcon />
+              <MicIcon />
+            </Box>
+          </Grid>
+
+          {/* MUI grid can be an item AND a container. */}
+          <Grid item container className="col-span-8">
+            <Container className="mb-24">
+              <Grid className="grid grid-cols-6 ml-5 space-x-5 space-y-5 ">
+                <div className="mt-5 ml-5">
+                  <User />
+                </div>
+                <User /> <User /> <User /> <User /> <User />
+                <User /> <User /> <User /> <User /> <User /> <User />
+              </Grid>
+            </Container>
+          </Grid>
+          <Grid item className="flex justify-end items-end col-span-2 px-8 ">
+            <Box
+              color={theme.palette.primary.dark}
+              className="flex flex-row space-x-5 "
+            >
+              <LibraryMusicIcon />
+              <PeopleIcon />
+              <ChatIcon />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </ThemeProvider>
   );
 }
