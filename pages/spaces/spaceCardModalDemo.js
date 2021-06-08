d49a03238e74c0455fc0691d6e69733748b5a749
edenchan@wawanesa.com <edenchan717@gmail.com>
2021-06-08 17:14:17 -0400

    Show a Space Card's Modal after clicking the Space Card

--- pages/spaces/spaceCardModalDemo.js
+++ pages/spaces/spaceCardModalDemo.js
@@ -10,11 +10,9 @@ import { Button } from "@material-ui/core";
 import SpaceCardModal from "../../components/Spaces/Dashboard/SpaceCardModal";
 import SpaceCard from "../../components/Spaces/Dashboard/SpaceCard";
 
-
 import Typography from "@material-ui/core/Typography";
 import { spaceCardModalTestData } from "../../data/spaceCardModalTestData";
 import { spaceCardTestData } from "../../data/spaceCardTestData";
-
 
 const theme = createMuiTheme({
   palette: {
@@ -30,12 +28,11 @@ const theme = createMuiTheme({
 });
 
 export default function SpaceCardModalDemo() {
-
-  // For the sake of the demo, we are using locally defined theme. 
+  // For the sake of the demo, we are using locally defined theme.
   // When using ThemeProvider, use the useTheme() hook.
   // const theme = useTheme();
   const { friends, participants, hosts } = spaceCardModalTestData;
-  const { cardData } = spaceCardTestData; 
+  const { cardData } = spaceCardTestData;
   const { spaceName, description, headCount, music } = cardData[0];
   const [open, setOpen] = useState(false);
 
@@ -46,14 +43,28 @@ export default function SpaceCardModalDemo() {
   const handleClose = () => {
     setOpen(false);
   };
+  
   return (
     <ThemeProvider theme={theme}>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        <Typography variant="body1" color="secondary">
-          Open Space Card Modal
-        </Typography>
-      </Button>
-      <SpaceCardModal open={open} onClose={handleClose} friends = {friends} participants={participants} hosts={hosts}>
+      <div
+        onClick={handleClickOpen}
+        className="cursor-pointer transform hover:scale-110 transition ease-out duration-200"
+      >
+        <SpaceCard
+          spaceName={spaceName}
+          description={description}
+          headCount={headCount}
+          music={music}
+        />
+      </div>
+
+      <SpaceCardModal
+        open={open}
+        onClose={handleClose}
+        friends={friends}
+        participants={participants}
+        hosts={hosts}
+      >
         <SpaceCard
           spaceName={spaceName}
           description={description}
