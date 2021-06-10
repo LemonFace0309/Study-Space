5fa20f2e520a3baf21e54e203b70b69efe260c7c
Mabel <mabelkwok322@gmail.com>
2021-06-10 01:34:41 -0400

    test again2

--- components/Cards/Card.js
+++ components/Cards/Card.js
@@ -3,7 +3,6 @@ import AllOutIcon from "@material-ui/icons/AllOut";
 import DoneIcon from "@material-ui/icons/Done";
 import EcoIcon from "@material-ui/icons/Eco";
 import LockIcon from "@material-ui/icons/Lock";
-
 // Card given from original template, might delete later if not needed
 const Style = "text-white text-xs";
 
