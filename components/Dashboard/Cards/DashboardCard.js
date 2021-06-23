693d1dcd9f9a89608a3c3fda99e520bd7b79ef2c
Eden Chan <edenchan717@gmail.com>
2021-06-23 11:33:09 -0400

    Arrange Space Card and Dashboard Card in Dashboard Container

--- components/Dashboard/Cards/DashboardCard.js
+++ components/Dashboard/Cards/DashboardCard.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Paper, Box, Typography, Grid } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 
@@ -8,23 +6,27 @@ export default function DashboardCard(props) {
   const theme = useTheme();
   const { spaceName, description, variant } = props;
   return (
-    <Paper className="rounded-xl" elevate={5}>
+    <Paper elevate={5} className="rounded-2xl">
       <Grid
+        className="rounded-2xl"
         container
-        // direction="col"
-        // alignContent="center"
-        // justify="flex-start"
         style={{
           background:
             variant === 'primary'
               ? theme.palette.primary.mainGradient
               : theme.palette.secondary.mainGradient,
         }}>
-        <Grid item>
-          <Box className="h-20 w-20 bg-gray-400"></Box>
+        <Grid item xs={3}>
+          <Box bgcolor="text.disabled" className="h-20 w-20 "></Box>
         </Grid>
         <Grid item>
-          <Box className="text-left p-3">
+          <Box
+            className="text-left p-3"
+            color={
+              variant === 'primary'
+                ? theme.palette.primary.contrastText
+                : theme.palette.primary.dark
+            }>
             <Typography color="text" variant="h5" align="left">
               {spaceName}
             </Typography>
--- components/Dashboard/DashboardContainer.js
+++ components/Dashboard/DashboardContainer.js
@@ -1,5 +1,12 @@
 import { React, useState } from 'react';
-import { Container, Grid, Typography } from '@material-ui/core';
+import {
+  Container,
+  Grid,
+  Typography,
+  Paper,
+  Box,
+  useTheme,
+} from '@material-ui/core';
 import DashboardCard from './Cards/DashboardCard';
 
 import SpaceCardModal from './Cards/SpaceCardModal';
@@ -10,79 +17,88 @@ import { spaceCardTestData } from '../../data/spaceCardTestData';
 const DashboardContainer = () => {
   const { friends, participants, hosts } = spaceCardModalTestData;
   const { cardData } = spaceCardTestData;
-
   const [open, setOpen] = useState(false);
 
-  return (
-    <Container>
-      <Grid container>
-        <Grid container item xs={12}>
-          <Grid item xs={12}>
-            <Typography variant="h4">Hey Charles!</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="h7">
-              The key is not to prioritize what's on your schedule, but to
-              schedule your priorities.
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container item xs={12}>
-          <Grid item xs={6}>
-            <DashboardCard
-              variant="primary"
-              spaceName="Create a Space"
-              description="insert some sort of tagline or feature description "
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <DashboardCard
-              spaceName="Join a Space"
-              description="insert some sort of tagline or feature description "
-            />
-          </Grid>
-        </Grid>
-        <Grid container item xs={12}>
-          {cardData.map(({ spaceName, description, headCount, music }) => {
-            return (
-              <Grid item xs={4}>
-                <div
-                  onClick={() => {
-                    setOpen(true);
-                  }}
-                  className="cursor-pointer transform hover:scale-110 transition ease-out duration-200">
-                  <SpaceCard
-                    spaceName={spaceName}
-                    description={description}
-                    headCount={headCount}
-                    music={music}
-                  />
-                </div>
+  const theme = useTheme();
 
-                <SpaceCardModal
-                  open={open}
-                  handleClose={() => {
-                    setOpen(false);
-                  }}
-                  friends={friends}
-                  participants={participants}
-                  hosts={hosts}>
-                  <SpaceCard
-                    spaceName={spaceName}
-                    description={description}
-                    headCount={headCount}
-                    music={music}
-                  />
-                </SpaceCardModal>
-              </Grid>
-            );
-          })}
+  return (
+    <Paper elevatation={6} className="rounded-3xl">
+      <Container>
+        <Grid container spacing={3}>
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={12}>
+              <Box color={theme.palette.primary.dark}>
+                <Typography variant="h4">Hey Charles!</Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box color={theme.palette.text.secondary}>
+                <Typography variant="h6">
+                  The key is not to prioritize what's on your schedule, but to
+                  schedule your priorities.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={6}>
+              <Typography variant="h6">
+               Need a space to study?
+              </Typography>
+              <DashboardCard
+                variant="primary"
+                spaceName="Create a Space"
+                description="insert some sort of tagline or feature description "
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <DashboardCard
+                spaceName="Join a Space"
+                description="insert some sort of tagline or feature description "
+              />
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} spacing={2}>
+            {cardData.map(({ spaceName, description, headCount, music }) => {
+              return (
+                <Grid item xs={4}>
+                  <div
+                    onClick={() => {
+                      setOpen(true);
+                    }}
+                    className="cursor-pointer transform hover:scale-110 transition ease-out duration-200">
+                    <SpaceCard
+                      spaceName={spaceName}
+                      description={description}
+                      headCount={headCount}
+                      music={music}
+                    />
+                  </div>
+
+                  <SpaceCardModal
+                    open={open}
+                    handleClose={() => {
+                      setOpen(false);
+                    }}
+                    friends={friends}
+                    participants={participants}
+                    hosts={hosts}>
+                    <SpaceCard
+                      spaceName={spaceName}
+                      description={description}
+                      headCount={headCount}
+                      music={music}
+                    />
+                  </SpaceCardModal>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Grid container item xs={12}></Grid>
         </Grid>
-        <Grid container item xs={12}>
-          
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Paper>
   );
 };
 export default DashboardContainer;
