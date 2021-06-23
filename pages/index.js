4a6327f94197e9af4c78302c04604b8c65127917
Charles Liu <charles.liu0309@gmail.com>
2021-06-23 04:12:30 -0400

    added simple image upload form on frontend

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,86 @@
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { getSession } from 'next-auth/client';
+import Button from '@material-ui/core/Button';
 
 import User from '../models/User';
 import dbConnect from '../utils/dbConnect';
 
-const Home = ({ session }) => {
+const Home = ({ session, acceptedFileTypes, allowMultipleFiles }) => {
   session = JSON.parse(session);
   console.debug(session);
 
-  return <h1>WELCOME :D</h1>;
+  const fileInputRef = useRef(null);
+  const formRef = useRef(null);
+
+  const handleUpdateProfile = async (e) => {
+    e.preventDefault();
+
+    if (!fileInputRef.current.files?.length) {
+      return;
+    }
+
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' },
+      onUploadProgress: (event) => {
+        console.log(
+          `Current progress:`,
+          Math.round((event.loaded * 100) / event.total)
+        );
+      },
+    };
+
+    const formData = new FormData();
+
+    Array.from(fileInputRef.current.files).forEach((file) => {
+      formData.append(fileInputRef.current.name, file);
+    });
+
+    // for (let key of formData.entries()) {
+    //   console.log(key[0] + ', ' + key[1]);
+    // }
+
+    const response = await axios.post(
+      '/api/profile/edit-profile',
+      formData,
+      config
+    );
+    console.debug(response);
+
+    formRef.current?.reset();
+  };
+
+  return (
+    <>
+      <h1>WELCOME :D</h1>
+      {session && (
+        <form className="mt-4" onSubmit={handleUpdateProfile} ref={formRef}>
+          <label htmlFor="image">Upload new Profile Pic</label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            ref={fileInputRef}
+            accept={acceptedFileTypes}
+            multiple={allowMultipleFiles}
+          />
+          <Button type="submit">Save</Button>
+        </form>
+      )}
+    </>
+  );
 };
 
 Home.propTypes = {
   session: PropTypes.string.isRequired,
+  acceptedFileTypes: PropTypes.string,
+  allowMultipleFiles: PropTypes.bool,
+};
+
+Home.defaultProps = {
+  acceptedFileTypes: '',
+  allowMultipleFiles: false,
 };
 
 export default Home;
