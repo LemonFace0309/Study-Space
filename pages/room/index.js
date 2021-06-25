b9dd5262c80dc831a64787123257a402e8bc311b
Charles Liu <charles.liu0309@gmail.com>
2021-06-25 01:51:24 -0400

    rebased onto master

--- pages/room/index.js
+++ pages/room/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { v1 as uuid } from 'uuid';
 import { useRouter } from 'next/router';
 
-<<<<<<< HEAD
-const CreateRoom = (props) => {
-=======
 const CreateRoom = () => {
->>>>>>> 61cddb4... adjusted eslint and tempoarily removed eslint requirement for prod builds
   const router = useRouter();
   function create() {
     const id = uuid();
