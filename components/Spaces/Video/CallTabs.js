83fd2296f095c9699d1287270bfcda0f3172ae8d
Jimmy Yang <68363001+jimmyygit@users.noreply.github.com>
2021-06-30 20:11:26 -0400

    Video call leave, mute, disable video. Call tabs (#72)
    * Video call leave, mute, disable video. Call tabs
    
    * Le suggestions
    
    * Format

---
+++ components/Spaces/Video/CallTabs.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+
+import { Tabs, TabPanel } from 'react-tabs';
+
+import { IconButton, Grid, Paper } from '@material-ui/core';
+import { Chat as ChatIcon, People, LibraryMusic, PlaylistAddCheck } from '@material-ui/icons';
+
+import Chat from '../Chat/'
+
+
+const callTabsIndex = {
+  EMPTY_TAB: 0,
+  MUSIC_QUEUE: 1,
+  MUSIC_LIBRARY: 2,
+  PEOPLE: 3,
+  CHAT: 4,
+}
+
+function CallTabs({
+  peersRef,
+  conversation,
+  setConversation,
+  showTabs,
+  setShowTabs,
+}) {
+
+  const [tabIndex, setTabIndex] = useState(callTabsIndex.CHAT);
+
+  function setTab(newTabIndex) {
+    if (newTabIndex === tabIndex) {
+      setTabIndex(callTabsIndex.EMPTY_TAB)
+      setShowTabs(false)
+    } else {
+      setTabIndex(newTabIndex)
+      setShowTabs(true)
+    }
+  }
+
+
+
+  return (
+    <>
+      {showTabs &&
+        <Grid item xs={12} md={4} className="h-full p-5 flex flex-col items-center justify-items-center">
+          <Tabs selectedIndex={tabIndex}>
+            {/* The empty tab  */}
+            <TabPanel></TabPanel>
+            <TabPanel>
+              <Paper elevation={2} className="w-90 h-full p-5 font-bold bg-white">Add Queue</Paper>
+            </TabPanel>
+            <TabPanel>
+              <Paper elevation={2} className="w-90 h-full p-5 font-bold bg-white">Mujic</Paper>
+            </TabPanel>
+            <TabPanel>
+              <Paper elevation={2} className="w-90 h-full p-5 font-bold bg-white">People</Paper>
+            </TabPanel>
+            <TabPanel>
+              <Chat
+                peersRef={peersRef}
+                conversation={conversation}
+                setConversation={setConversation}
+              />
+            </TabPanel>
+          </Tabs>
+        </Grid>
+      }
+
+      <div className="absolute bottom-0 right-0">
+        <IconButton onClick={() => setTab(callTabsIndex.MUSIC_QUEUE)}>
+          <PlaylistAddCheck />
+        </IconButton>
+        <IconButton onClick={() => setTab(callTabsIndex.MUSIC_LIBRARY)}>
+          <LibraryMusic />
+        </IconButton>
+        <IconButton onClick={() => setTab(callTabsIndex.PEOPLE)}>
+          <People />
+        </IconButton>
+        <IconButton onClick={() => setTab(callTabsIndex.CHAT)}>
+          <ChatIcon />
+        </IconButton>
+      </div>
+
+    </>
+  );
+}
+
+export default CallTabs;
