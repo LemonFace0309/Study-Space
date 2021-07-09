1ac27d9f5b5ccc0f4568e74a1c9cb1c9384a65d7
Eden Chan <edenchan717@gmail.com>
2021-07-09 09:26:57 -0400

    Resolve SpacePackage ESLint error with PropTypes

--- components/Dashboard/Cards/SpacePackage.js
+++ components/Dashboard/Cards/SpacePackage.js
@@ -3,10 +3,11 @@ import { useState } from 'react';
 import SpaceCardModal from './SpaceCardModal';
 import SpaceCard from '../../Shared/SpaceCard';
 
-const SpacePackage = ({ data }) => {
+const SpacePackage = ({ spaceCardData, spaceCardModalData }) => {
   const [open, setOpen] = useState(false);
 
-  const { spaceName, description, headCount, music, friends, participants, hosts } = data;
+  const { spaceName, description, headCount, music } = spaceCardData;
+  const { friends, participants, hosts } = spaceCardModalData;
   return (
     <>
       <div
@@ -36,17 +37,17 @@ const SpacePackage = ({ data }) => {
 };
 
 SpacePackage.propTypes = {
-  isClickable: PropTypes.bool,
-  spaceName: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  headCount: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-  music: PropTypes.string.isRequired,
-  handleClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  children: PropTypes.element.isRequired,
-  friends: PropTypes.array,
-  participants: PropTypes.array,
-  hosts: PropTypes.array,
+  spaceCardData: PropTypes.shape({
+    spaceName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    headCount: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    music: PropTypes.string.isRequired,
+  }),
+  spaceCardModalData: PropTypes.shape({
+    friends: PropTypes.array,
+    participants: PropTypes.array,
+    hosts: PropTypes.array,
+  }),
 };
 
 export default SpacePackage;
