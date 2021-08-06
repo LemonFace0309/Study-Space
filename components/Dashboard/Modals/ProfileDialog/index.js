a511ed1e154cf27115c992cdcbd7e5dc5930fc07
Mabel <mabelkwok322@gmail.com>
2021-08-06 01:10:17 -0400

    fix mobile settings tab nav ui

--- components/Dashboard/Modals/ProfileDialog/index.js
+++ components/Dashboard/Modals/ProfileDialog/index.js
@@ -13,6 +13,8 @@ import CloseIcon from '@material-ui/icons/Close';
 import EditIcon from '@material-ui/icons/Edit';
 import CheckIcon from '@material-ui/icons/Check';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Avatar from '@material-ui/core/Avatar';
 
 import AccountDetails from './AccountDetails';
 import ChangePassword from './ChangePassword';
@@ -47,6 +49,12 @@ const useStyles = makeStyles((theme) => ({
   },
   tabTitle: {
     color: theme.palette.primary.dark,
+    [theme.breakpoints.up('md')]: {
+      paddingBottom: '25px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2rem',
+    },
   },
   closeIcon: {
     position: 'absolute',
@@ -57,6 +65,50 @@ const useStyles = makeStyles((theme) => ({
   containedPrimary: {
     background: theme.palette.primary.dark,
     borderRadius: '2rem',
+  },
+  imageContainer: {
+    width: theme.spacing(32),
+    height: theme.spacing(32),
+    [theme.breakpoints.down('md')]: {
+      width: theme.spacing(24),
+      height: theme.spacing(24),
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+    overflow: 'hidden',
+    borderRadius: '50%',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageOverlay: {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: theme.palette.primary.contrastText,
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
+    transition: 'all 0.2s ease-in-out',
+    '& > h6': {
+      display: 'none',
+      transition: 'all 0.2s ease-in-out',
+    },
+    '&:hover': {
+      background: 'rgba(0,0,0,0.4)',
+      '& > h6': {
+        display: 'block',
+      },
+      '& > input': {
+        cursor: 'pointer',
+      },
+    },
   },
 }));
 
@@ -130,7 +182,7 @@ const ProfileDialog = ({ session, isOpen, handleClose, tabs }) => {
   };
 
   const toggleSettings = () => {
-    setShowSettings(prev => !prev)
+    setShowSettings((prev) => !prev);
   };
 
   return (
@@ -179,44 +231,81 @@ const ProfileDialog = ({ session, isOpen, handleClose, tabs }) => {
                 {/* Settings Nav for Small Screens */}
                 {showSettings ? (
                   <Hidden mdUp>
-                    <Grid container item xs={12} md={3} className="p-4">
-                      <TabList className="w-full">
-                        {tabs.map((tab, index) => (
-                          <Tab key={tab.tabName} className="pl-2">
-                            <Button
-                              fullWidth
-                              disableRipple
-                              onClick={toggleSettings}
-                              className={classNames([
-                                'justify-start',
-                                'pb-2',
-                                'pl-4',
-                                classes.tab,
-                                index === tabIndex && classes.activeTab,
-                              ])}>
-                              {tab.tabName}
-                            </Button>
-                          </Tab>
-                        ))}
-                      </TabList>
-                      <Grid container direction="column" alignItems="baseline" justify="flex-end">
-                        <Button>Privacy</Button>
-                        <Button>Logout</Button>
+                    <Grid container alignItems="middle" item xs={12} md={3} className="p-4">
+                      <Grid item xs={12} sm={12}>
+                        <Typography align="left" variant="h3" className={classes.title}>
+                          Settings
+                        </Typography>
+                        <CloseIcon className={classes.closeIcon} />
+                        <Grid container item sm={12} justify="center" alignItems="center">
+                          <div className={classes.imageContainer}>
+                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                            <img
+                              alt="Your Profile Pic"
+                              src={null ?? session?.user?.image}
+                              className={classes.largeAvatar}
+                            />
+                          </div>
+                        </Grid>
+                        <Typography align="center" variant="h5" className={classes.title}>
+                          {session?.user?.name}
+                        </Typography>
+
+                        {/* Tabs */}
+                        <TabList>
+                          {tabs.map((tab, index) => (
+                            <Tab key={tab.tabName} className="pl-2">
+                              <Button
+                                fullWidth
+                                disableRipple
+                                onClick={toggleSettings}
+                                endIcon={<ArrowForwardIosIcon />}
+                                className={classNames([
+                                  'pb-2',
+                                  'pl-4',
+                                  classes.tab,
+                                  index === tabIndex && classes.activeTab,
+                                ])}>
+                                {tab.tabName}
+                              </Button>
+                            </Tab>
+                          ))}
+                        </TabList>
                       </Grid>
+                      <Hidden mdUp>
+                        <Grid container direction="column" justify="middle">
+                          <Button>Privacy</Button>
+                          <Button>Logout</Button>
+                        </Grid>
+                      </Hidden>
+                      <Hidden smDown>
+                        <Grid container direction="column" alignItems="baseline" justify="flex-end">
+                          <Button>Privacy</Button>
+                          <Button>Logout</Button>
+                        </Grid>
+                      </Hidden>
                     </Grid>
                   </Hidden>
                 ) : (
                   /* GREY AREA */
                   <Grid container item direction="row" xs={12} className="p-4">
-                    <Hidden mdUp>
-                      <ArrowBackIosIcon onClick={toggleSettings}></ArrowBackIosIcon>
-                    </Hidden>
-                    <Typography variant="h3" gutterBottom className={classes.tabTitle}>
-                      {tab.title}
-                    </Typography>
-                    <Hidden smDown>
-                      <CloseIcon className={classes.closeIcon} />
-                    </Hidden>
+                    <Grid container direction="row" alignItems="center" justify="flex-start">
+                      <Hidden mdUp>
+                        <Grid item xs={3}>
+                          <ArrowBackIosIcon onClick={toggleSettings}></ArrowBackIosIcon>
+                        </Grid>
+                      </Hidden>
+                      <Grid item xs={8}>
+                        <Typography variant="h3" className={classes.tabTitle}>
+                          {tab.title}
+                        </Typography>
+                      </Grid>
+                      <Hidden smDown>
+                        <Grid item>
+                          <CloseIcon className={classes.closeIcon} />
+                        </Grid>
+                      </Hidden>
+                    </Grid>
                     <Grid item xs={12} className={classes.mainFormBody}>
                       {renderTabComponent(tab.component)}
                     </Grid>
@@ -224,7 +313,7 @@ const ProfileDialog = ({ session, isOpen, handleClose, tabs }) => {
                 )}
               </TabPanel>
             ))}
-            {!showSettings &&
+            {!showSettings && (
               <Grid container item justify="flex-end" className="p-4">
                 <Button
                   variant="contained"
@@ -236,7 +325,7 @@ const ProfileDialog = ({ session, isOpen, handleClose, tabs }) => {
                   {editMode ? 'Save' : 'Edit Profile'}
                 </Button>
               </Grid>
-            }
+            )}
           </Grid>
         </Grid>
       </Tabs>
