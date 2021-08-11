4aafeaa5e8afd85787bcc9573905d0892b0e87a5
Eden Chan <edenchan717@gmail.com>
2021-08-10 21:14:21 -0400

    Add default fields for SSO sign up users for next-auth to put into database

--- models/NextAuthUser.js
+++ models/NextAuthUser.js
@@ -1,13 +1,19 @@
+// This file overrides the custom defined User Schema Next Auth puts into our database when a user signs in with SSO
 import Adapters from 'next-auth/adapters';
 
 // Extend the built-in models using class inheritance
 export default class User extends Adapters.TypeORM.Models.User.model {
   // You can extend the options in a model but you should not remove the base
   // properties or change the order of the built-in options on the constructor
-  customField;
+
+  username = '';
+  phoneNumber = '';
+  password = '';
+  friends = [];
+  type = 'credentials';
+  __v = 0;
   constructor(name, email, image, emailVerified) {
     super(name, email, image, emailVerified);
-    this.customField = 'test';
   }
 }
 
@@ -16,10 +22,23 @@ export const UserSchema = {
   target: User,
   columns: {
     ...Adapters.TypeORM.Models.User.schema.columns,
-    // Adds a phoneNumber to the User schema
-    customField: {
-      type: 'varchar',
-      nullable: true,
-    },
+
+    // We don't need these, they're already defined with our Mongoose User model
+    // username: {
+    //   type: 'varchar',
+    //   nullable: true,
+    // },
+    // phoneNumber: {
+    //   type: 'varchar',
+    //   nullable: true,
+    // },
+    // password: {
+    //   type: 'varchar',
+    //   nullable: true,
+    // },
+    // friends: {
+    //   type: 'object',
+    //   nullable: true,
+    // },
   },
 };
