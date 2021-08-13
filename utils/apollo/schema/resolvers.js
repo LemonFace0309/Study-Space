9ca55c0f33f370f11632ae5178fe1474f466165c
LemonFace0309 <charles.liu0309@gmail.com>
2021-08-13 04:30:31 -0400

    hot fixes

--- utils/apollo/schema/resolvers.js
+++ utils/apollo/schema/resolvers.js
@@ -4,8 +4,8 @@ import Space from 'models/Spaces';
 export const resolvers = {
   Query: {
     users: async (parent, args, context) => {
-      console.debug('args', args, 'userIds', userIds);
       const { userIds, email, name } = args;
+      console.debug('args', args, 'userIds', userIds);
       const data = [];
 
       await dbConnect;
@@ -18,22 +18,30 @@ export const resolvers = {
       }
 
       // For Query by ID
-      for (let _id of userIds) {
-        const user = await User.findOne({ _id });
-        data.push(user);
+      for (let id of userIds) {
+        try {
+          const user = await User.findById(id);
+          data.push(user);
+        } catch (err) {
+          console.debug(err);
+        }
       }
       return await data;
     },
 
     spaces: async (parent, args, context) => {
-      console.debug('args', args, 'spaceIds', spaceIds);
       const { spaceIds } = args;
+      console.debug('args', args, 'spaceIds', spaceIds);
       const data = [];
 
       await dbConnect;
       for (let spaceId of spaceIds) {
-        const space = await Space.findOne({ spaceId });
-        data.push(space);
+        try {
+          const space = await Space.findOne({ spaceId });
+          data.push(space);
+        } catch (err) {
+          console.debug(err);
+        }
       }
       return data;
     },
