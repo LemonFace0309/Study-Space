0fa43f7aca408c9a8107db94daa1be697ba0df77
Eden Chan <edenchan717@gmail.com>
2021-08-18 19:29:59 -0400

    Add comments

--- utils/apollo/schema/index.js
+++ utils/apollo/schema/index.js
@@ -2,6 +2,7 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './typedefs';
 import { resolvers } from './resolvers';
 
+// This is only used in apollo/client because it has to take in a makeExectuableSchema type, and not api/graphql.js
 export const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
