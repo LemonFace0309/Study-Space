44011862aa2672dc3b9ce6e57718ddc1fd60a927
Charles Liu <charles.liu0309@gmail.com>
2021-09-27 00:40:36 -0400

    updated callback urls after signing or loging in

--- components/Auth/AuthDialog.js
+++ components/Auth/AuthDialog.js
@@ -72,16 +72,12 @@ const AuthDialog = ({
               <div className="flex w-full justify-center items-center">
                 <IconButton
                   className="mr-4 outline-none"
-                  onClick={() =>
-                    signIn(providers?.facebook.id, { callbackUrl: `${process.env.NEXT_PUBLIC_URL}/dashboard` })
-                  }>
+                  onClick={() => signIn(providers?.facebook.id, { callbackUrl: '/dashboard' })}>
                   <Image src="/images/facebook.svg" alt="Facebook Login" height="32" width="32" />
                 </IconButton>
                 <IconButton
                   className="outline-none"
-                  onClick={() =>
-                    signIn(providers?.google.id, { callbackUrl: `${process.env.NEXT_PUBLIC_URL}/dashboard` })
-                  }>
+                  onClick={() => signIn(providers?.google.id, { callbackUrl: '/dashboard' })}>
                   <Image src="/images/google.svg" alt="Facebook Login" height="32" width="32" />
                 </IconButton>
               </div>
