a3f99403523b43701264c2540551ebee585d520e
Charles Liu <charles.liu0309@gmail.com>
2021-10-27 04:17:40 -0400

    updated package for next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'es', 'de', 'zh', 'ja', 'ko'],
+    // locales: ['en', 'fr', 'es', 'de', 'zh', 'ja', 'ko'],
+    locales: ['en', 'fr'],
     localePath: path.resolve('./public/locales'),
   },
 };
